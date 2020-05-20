9a0a6016c2d14366d34ed8edbe006cd896d5dc81
shahzaib199579 <shahzaib199579@gmail.com>
2020-05-20 15:44:21 +0500

    Initial commit

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+
+  final Function onTap;
+  final String buttonTitle;
+
+  BottomButton({@required this.buttonTitle, @required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        padding: EdgeInsets.only(bottom: 20),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,67 @@
+import '../components/reusable_card.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../components/bottom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
+
+  ResultsPage({@required this.bmiResult, @required this.resultText, @required this.interpretation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(15),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              cardColor: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    resultText,
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(buttonTitle: 'RE-CALCULATE', onTap: () {
+            Navigator.pop(context);
+          },)
+        ],
+      ),
+    );
+  }
+}
